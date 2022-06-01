eafcaab60af3357cab49c9e0ea5afd4b7751947f
Chris LaRose <cjlarose@gmail.com>
2022-06-01 16:41:56 -0700

    Rename user to cjlarose
    Also update password & SSH authorized keys

--- users/cjlarose/nixos.nix
+++ users/cjlarose/nixos.nix
@@ -4,14 +4,14 @@
   # https://github.com/nix-community/home-manager/pull/2408
   environment.pathsToLink = [ "/share/fish" ];
 
-  users.users.mitchellh = {
+  users.users.cjlarose = {
     isNormalUser = true;
-    home = "/home/mitchellh";
+    home = "/home/cjlarose";
     extraGroups = [ "docker" "wheel" ];
     shell = pkgs.fish;
-    hashedPassword = "$6$p5nPhz3G6k$6yCK0m3Oglcj4ZkUXwbjrG403LBZkfNwlhgrQAqOospGJXJZ27dI84CbIYBNsTgsoH650C1EBsbCKesSVPSpB1";
+    hashedPassword = "$6$ka5KPFwZT2Z2HNTa$EqMm1FPldxkaOVKBUkcekJ9rqt/5hTine7I1K6JlxH49YJASSu5p6nfJSzwenbGo6m71T/pT2UdvW0E1ZYbgv/";
     openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGbTIKIPtrymhvtTvqbU07/e7gyFJqNS4S0xlfrZLOaY mitchellh"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGFtA/9w60OssA+Eji+Ygvd1XCJk/zw/uYLdiiaevELu chris.larose@picktrace.com"
     ];
   };
 
